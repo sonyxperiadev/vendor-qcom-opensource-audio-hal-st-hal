58e1b48d2fcfff36ea2f6214daf60c20f37b92c8
Zhou Song <zhous@codeaurora.org>
2019-12-20 10:21:07 +0800

    st-hal: force to reset backend for device switch
    Device reset can be ignored during device switch as it's
    consecutively needed by audio recording during concurrency.
    For example, when VA switches device from dual mic to
    headset mic, path of dual mic won't get reset, so mixer
    ctl like "VA_CDC_DMA_TX_0 Channels" is still left as 2.
    To fix this, set the flag to reset previous device though
    it is still needed by audio recording, as recording will
    finally switch to the same device as VA.
    
    Change-Id: If3269a703e9d43de30e4521c1f7f130b5cc749d5

--- sound_trigger_hw.c
+++ sound_trigger_hw.c
@@ -1113,6 +1113,7 @@ static void handle_device_switch(bool connect, audio_event_info_t* config)
      * state so device switch will set up the next session with the
      * new device to be started later
      */
+    stdev->reset_backend = true;
     switch_device();
 
     pthread_mutex_unlock(&stdev->lock);
